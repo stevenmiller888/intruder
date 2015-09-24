4944cb7cfa3eceffbce5c5e0ac3631a6c2c04b2e
Steven Miller <stevenmiller888@me.com>
2015-09-23 19:07:43 -0700

    added decrypt function to execute on an interval

--- test/index.js
+++ test/index.js
@@ -24,4 +24,21 @@ describe('Intruder()', function() {
     var intruder = Intruder();
     assert(intruder instanceof Intruder);
   });
+
+  it('should take an object of options', function() {
+    it('should take an `interval` option', function() {
+      var intruder = Intruder({ interval: 20000 });
+      assert.equal(intruder.interval, 20000);
+    });
+
+    it('should take a `channel` option', function() {
+      var intruder = Intruder({ channel: 20 });
+      assert.equal(intruder.channel, 20);
+    });
+
+    it('should not take a `foo` option', function() {
+      var intruder = Intruder({ foo: 20 });
+      assert(!intruder.foo);
+    });
+  });
 });
